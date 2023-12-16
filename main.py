37f2bb6f0cf054e327e94dee1eb619dd57a7a9e5
Bastian Stark <53190965+bastian-stark@users.noreply.github.com>
2023-12-15 17:17:13 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+#Caesar's Cipher Encryption/Decryption App
+
 def encode(message, shift):
+    """Encode a message using Caesar's Cipher"""
     newMessage = ''
     for letter in message:
         position = alphabet.index(letter)
@@ -8,6 +11,7 @@ def encode(message, shift):
     print(f"Message: {newMessage}")
 
 def decode(message, shift):
+    """Decode a message using Caesar's Cipher"""
     newMessage = ''
     for letter in message:
         position = alphabet.index(letter)
@@ -16,14 +20,15 @@ def decode(message, shift):
         newMessage += newLetter
     print(f"Message: {newMessage}")
 
+#Intro
 print('Welcome to the encoder.')
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+#Inputs
 choiceEnDe = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 message = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+#Encode/Decode message
 if choiceEnDe == "encode":
     word = encode(message, shift)
 else:
